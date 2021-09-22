a4a7161bd854047b4c6d9ebcec5b31c802f5d9e0
David Knight <dave@mostlyspam.com>
2021-09-22 15:41:01 -0700

    Moved Title into code behind.

---
+++ Pages/Index.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorMinimal.Pages
+{
+    public partial class Index
+    {        string title = "Client-side (Web Assembly) Blazor Minimal";
+    }
+}

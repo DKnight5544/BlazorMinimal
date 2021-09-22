17a7269f7ae5e736e438c1c8565e1339efcc1846
David Knight <dave@mostlyspam.com>
2021-09-22 15:45:47 -0700

    moved function to code behind.

--- Pages/Index.razor.cs
+++ Pages/Index.razor.cs
@@ -6,6 +6,12 @@ using System.Threading.Tasks;
 namespace BlazorMinimal.Pages
 {
     public partial class Index
-    {        string title = "Client-side (Web Assembly) Blazor Minimal";
+    {        
+        string title = "Client-side (Web Assembly) Blazor Minimal";
+        string GetTextFromMethodInClass()
+        {
+            return "This text is derived from a C# .cs file within the assembly";
+        }
+
     }
 }
--- Pages/IndexBase.cs
+++ Pages/IndexBase.cs
@@ -4,9 +4,6 @@ namespace BlazorMinimal.Pages
 {
     public class IndexBase : LayoutComponentBase
     {
-        public string GetTextFromMethodInClass()
-        {
-            return "This text is derived from a C# .cs file within the assembly";
-        }
+
     }
 }
